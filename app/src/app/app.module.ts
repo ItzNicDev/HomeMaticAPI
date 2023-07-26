af266343088a60a081b0f2bccb26a3817fad4be8
niclaspirkl <nimaxpirkl@gmail.com>
2023-07-26 19:13:02 +0200

    Added Camera-Widget

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
+import { WatertankComponent } from './components/watertank/watertank.component';
+import { CamerasComponent } from './components/cameras/cameras.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WatertankComponent,
+    CamerasComponent
   ],
   imports: [
     BrowserModule,
